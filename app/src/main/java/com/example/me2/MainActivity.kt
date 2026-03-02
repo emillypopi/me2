4040125daeeac788ba3512783857fff934dd02f8
emillypopi <emillyshirley6@gmail.com>
2026-03-02 23:43:13 +0300

    final commit for now

--- app/src/main/java/com/example/me2/MainActivity.kt
+++ app/src/main/java/com/example/me2/MainActivity.kt
@@ -1,20 +1,30 @@
 package com.example.me2
 
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
-import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val editName = findViewById<EditText>(R.id.editName)
+        val textMessage = findViewById<TextView>(R.id.textMessage)
+        val buttonSubmit = findViewById<Button>(R.id.buttonSubmit)
+        val buttonClear = findViewById<Button>(R.id.buttonClear)
+
+        buttonSubmit.setOnClickListener {
+            val name = editName.text.toString()
+            textMessage.text = getString(R.string.welcome_message, name)
+        }
+        buttonClear.setOnClickListener {
+            editName.text.clear()
+            textMessage.text = ""
         }
     }
 }
